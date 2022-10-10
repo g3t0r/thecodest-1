4af9ea5600d54cc8d64bb44e88d735eeaa310aed
Jan Jakubowski <jan.jakubowski.ti@gmail.com>
2022-10-10 21:36:03 +0200

    Initial commit

---
+++ src/main/java/com/janjakubowski/thecodest/ThecodestApplication.java
@@ -0,0 +1,13 @@
+package com.janjakubowski.thecodest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThecodestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThecodestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/janjakubowski/thecodest/ThecodestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janjakubowski.thecodest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThecodestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
